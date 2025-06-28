6b9f093a0ff966d206bba2caf9bfe929c83a9400
nico-schoeman <9889916+nico-schoeman@users.noreply.github.com>
2025-06-28 11:46:21 +0200

    Command line that takes 4 numbers and outputs them

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        Argument<int[]> numbers = new("Input Numbers")
+        {
+            Description = "The input numbers to generate a equasion for to equal 24.",
+            Arity = new ArgumentArity(4, 4)
+        };
+
+        RootCommand rootCommand = new("24 Game");
+        rootCommand.Arguments.Add(numbers);
+
+        rootCommand.SetAction(parseResult =>
+        {
+            int[] result = parseResult.GetValue(numbers);
+            Console.WriteLine($"{String.Join(",", result)}");
+            return 0;
+        });
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
+    }
+}
